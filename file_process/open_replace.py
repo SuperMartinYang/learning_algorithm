c4656b6f14b8a60d75d9cc4e3de11de01a17c617
SuperYang <shy58@pitt.edu>
2017-11-07 02:17:03 -0500

    ignore

--- file_process/open_replace.py
+++
@@ -1,4 +0,0 @@
-read_f = open("H:\\课程学习\\Information Security & Privacy\\homework\\code\\1.txt","rU");
-write_f = open("H:\\课程学习\\Information Security & Privacy\\homework\\code\\2.txt","a+");
-content = read_f.read()
-write_f.write(content.replace('\n',','))
